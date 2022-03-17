d0b77edda441c70ab44b44cd1ea26f96ac271009
Daniela-MMO <danni.oliver94@gmail.com>
2022-03-16 23:15:33 -0300

    subindo projeto

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+    <ul>
+      <li v-for="foto of fotos">
+        <img :src="foto.url" :alt="foto.titulo" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      titulo: "Alurapic",
+      fotos: []
+    };
+  },
+  created() {
+    //para invocar os hooks
+    let promise = this.$http.get("http://localhost:3000/v1/fotos");
+    promise.then(res => {
+      res.json().then(fotos => (this.fotos = fotos));
+    });
+  }
+};
+</script>
+
+<style></style>
